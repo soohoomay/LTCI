7bc8a475cf6083159cdb9c015b8a010a7c3e94a3
pauline <lee6260lee@yahoo.com>
2016-11-30 16:17:27 -0800

    Add files via upload

---
+++ Ex10.rb
@@ -0,0 +1,22 @@
+tabby_cat = "\tI'm tabbed in."
+# learning about tabbed in
+persian_cat = "I'm split\non a line."
+# learning about split
+backslash_cat = "I'm \\ a \\ cat."
+# learning about backslash
+
+fat_cat = """
+# using 3 quotes
+I'll do a list:
+\t* Cat food
+#using tabbed in
+\t* Fishies
+#using tabbed in
+\t* Catmop\n\t* Grass
+#using tabbed in and backslash
+"""
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
+#commands
